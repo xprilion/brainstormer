7bc795589dee57fa0339adc6b33cee55892c88a7
Anubhav Singh <anubhav.ssd@gmail.com>
2017-10-24 02:36:56 +0530

    menu buttons animate

--- js/script.js
+++ js/script.js
@@ -2,13 +2,34 @@ $(document).ready(function(){
 
 	$('body').parallax({imageSrc: 'img/zengb-1.jpg'});
 
+	var container = document.querySelector('#masonry');
+	var msnry = new Masonry( container, {
+		columnWidth: 50,
+		itemSelector: '.item'
+	});
 
 
 	var waypoints = $('#photos').waypoint({
 		handler: function(direction) {
 			console.log(this.element.id + ' hit' + direction)
 		}
-	})
+	});
 
+	$(document).on('click', 'a[href^="#"]', function (event) {
+	    event.preventDefault();
+
+	    $('html, body').animate({
+	        scrollTop: $($.attr(this, 'href')).offset().top
+	    }, 500);
+	});
+
+
+	$('.animated-bg-btn').mouseenter(function(){
+		$(this).find('span').find('p').slideDown();
+	});
+
+	$('.animated-bg-btn').mouseleave(function(){
+		$(this).find('span').find('p').slideUp();
+	});
 
 });
