9f30d0efb092d5a18f90c50c2a59fb6f9dae00cd
Anubhav Singh <anubhav.ssd@gmail.com>
2017-10-24 02:07:40 +0530

    button backgrounds

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 
+	$('body').parallax({imageSrc: 'img/zengb-1.jpg'});
 
 
+
+	var waypoints = $('#photos').waypoint({
+		handler: function(direction) {
+			console.log(this.element.id + ' hit' + direction)
+		}
+	})
 
 
 });
