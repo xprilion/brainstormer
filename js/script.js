9e2c337ceebd0cc99f54945cce77221f2637712f
Anubhav Singh <anubhav.ssd@gmail.com>
2017-10-29 11:50:03 +0530

    95%

--- js/script.js
+++ js/script.js
@@ -3,35 +3,7 @@ $(document).ready(function(){
 	var colors = ["green", "purple", "brown", "red", "violet"];
 	var nowCol = 1;
 
-	var container = document.querySelector('#masonry');
-	var msnry = new Masonry( container, {
-		columnWidth: 50,
-		itemSelector: '.item'
-	});
-
 	// Logo Animation Sequence
-
-	$('#logoContainer').css('opacity', '1').addClass('animated bounceInDown');
-
-	$('#logoContainerMobile').css('opacity', '1').addClass('animated bounceInDown');
-
-	setTimeout(function(){ $('#logoContainer').addClass('doExpand'); }, 1000);
-	setTimeout(function(){ $('#logoText').css('opacity', '1').textillate({ in: { effect: 'rollIn' }});  }, 1750);
-
-	var bgChanger = setInterval(function(){
-		$('.logoImg').addClass('spinner');
-		$('#navbarLogo').addClass('spinner');
-		setTimeout(function(){
-			$('.logoImg').removeClass('spinner');
-			$('#navbarLogo').removeClass('spinner');
-			nextCol = nowCol + 1;
-			if(nowCol==4){
-				nextCol = 0;
-			}
-			$('body').removeClass('bg-'+colors[nowCol]).addClass('bg-'+colors[nextCol]);
-			nowCol = nextCol;
-		}, 1500);
-	}, 5000);
 
 	var headWaypoint = $('header').waypoint({
 		handler: function(direction) {
@@ -63,3 +35,27 @@ $(document).ready(function(){
 	});
 
 });
+
+function pageScript(){
+	$('#logoContainer').css('opacity', '1').addClass('animated bounceInDown');
+
+	$('#logoContainerMobile').css('opacity', '1').addClass('animated bounceInDown');
+
+	setTimeout(function(){ $('#logoContainer').addClass('doExpand'); }, 1000);
+	setTimeout(function(){ $('#logoText').css('opacity', '1').textillate({ in: { effect: 'rollIn' }});  }, 1750);
+
+	var bgChanger = setInterval(function(){
+		$('.logoImg').addClass('spinner');
+		$('#navbarLogo').addClass('spinner');
+		setTimeout(function(){
+			$('.logoImg').removeClass('spinner');
+			$('#navbarLogo').removeClass('spinner');
+			nextCol = nowCol + 1;
+			if(nowCol==4){
+				nextCol = 0;
+			}
+			$('body').removeClass('bg-'+colors[nowCol]).addClass('bg-'+colors[nextCol]);
+			nowCol = nextCol;
+		}, 1500);
+	}, 5000);
+}
